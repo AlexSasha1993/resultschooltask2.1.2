5cb551f21eeb35f951c0f1f52c38f6bec712e2e3
Alexander <3724198@mail.ru>
2023-08-21 23:21:22 +0300

    Calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
 import './App.css';
 import { useState } from 'react';
 
-const numbers = [
-	{ id: 'a', name: 0 },
-	{ id: 'b', name: 1 },
-	{ id: 'c', name: 2 },
-	{ id: 'd', name: 3 },
-	{ id: 'e', name: 4 },
-	{ id: 'f', name: 5 },
-	{ id: 'g', name: 6 },
-	{ id: 'h', name: 7 },
-	{ id: 'i', name: 8 },
-	{ id: 'j', name: 9 },
-	{ id: 'k', name: '+' },
-	{ id: 'l', name: '-' },
-	{ id: 'm', name: '=' },
-	{ id: 'n', name: 'C' },
-];
-
-const onClick = () => {};
-
 export const App = () => {
+	const [display, setDisplay] = useState('');
+	const [isResult, setIsResult] = useState(false);
+	const [firstNumber, setFirstNumber] = useState(0);
+	const [currentOperation, setCurrentOperation] = useState('');
+
+	const handleNumberClick = (number) => {
+		if (isResult) {
+			setDisplay(number);
+			setIsResult(false);
+		} else {
+			setDisplay(display + number);
+		}
+	};
+
+	const handleOperationClick = (operation) => {
+		if (operation === '=') {
+			if (firstNumber !== '' && display !== '') {
+				const num1 = parseInt(firstNumber);
+				const num2 = parseInt(display);
+				let result;
+				if (currentOperation === '+') {
+					result = num1 + num2;
+				} else if (currentOperation === '-') {
+					result = num1 - num2;
+				}
+				setDisplay(result.toString());
+				setFirstNumber('');
+				setIsResult(true);
+			}
+		} else if (operation === 'C') {
+			setDisplay('');
+			setFirstNumber('');
+			setIsResult(false);
+		} else {
+			setFirstNumber(display);
+			setDisplay('');
+			setCurrentOperation(operation);
+			setIsResult(false);
+		}
+	};
+
 	return (
-		<div className="App">
-			<div className="output">
-				<input></input>
+		<div className="calculator">
+			<div className="display" style={{ color: isResult ? 'green' : 'black' }}>
+				{display}
 			</div>
-			{numbers.map(({ id, name }) => (
-				<button key={id}>{name}</button>
-			))}
+			<div className="buttons">
+				{[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+					<button key={number} onClick={() => handleNumberClick(number)}>
+						{number}
+					</button>
+				))}
+				<button onClick={() => handleOperationClick('+')}>+</button>
+				<button onClick={() => handleOperationClick('-')}>-</button>
+				<button onClick={() => handleOperationClick('=')}>=</button>
+				<button onClick={() => handleOperationClick('C')}>C</button>
+			</div>
 		</div>
 	);
 };
+
+export default App;
